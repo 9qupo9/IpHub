9dd2df35fe13e1d11d40e1ecc50840929160f79c
Tenespir <Tenespir@gmail.com>
2025-10-29 22:56:50 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "Index.html")
+			return
+		}
+		http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+	})
 	http.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir("./media"))))
 	http.ListenAndServe(":8080", nil)
 }
