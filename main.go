354ee58d7f5f535f4517f3925d5f4ca744d2d071
Tenespir <Tenespir@gmail.com>
2025-10-30 18:40:54 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	серв := http.NewServeMux()
+
+	серв.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir("./media"))))
+
+	серв.HandleFunc("/", func(отвечающий http.ResponseWriter, запрос *http.Request) {
+		http.ServeFile(отвечающий, запрос, "Index.html")
+	})
+
+	http.ListenAndServe(":8080", серв)
 }
