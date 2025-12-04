fa2bfe4b4113718549abb07c172fa3ecd1723019
User Name <user@example.com>
2025-12-04 17:39:48 +0200

    Initial commit with updated EducationAbout.js - added timeline improvements and new team member

---
+++ DeveloperArticles.js
@@ -0,0 +1,113 @@
+class DeveloperArticles {
+    constructor() {
+        this.develArticles = [
+            {
+                id: 1,
+                title: 'Developer Office Hours Recap by RamzesVII 20.11',
+                image: 'DevelArticles/cart00.png',
+                link: 'https://x.com/r_ladik/status/1991567163472376255'
+            },
+            {
+                id: 2,
+                title: 'Developer Office Hours Recap by RamzesVII 17.11',
+                image: 'DevelArticles/cart0.png',
+                link: 'https://x.com/r_ladik/status/1990478990738665633'
+            },
+            {
+                id: 3,
+                title: 'Developer Office Hours Recap by RamzesVII 06.11',
+                image: 'DevelArticles/cart1.png',
+                link: 'https://x.com/r_ladik/status/1986479041189494814'
+            },
+            {
+                id: 4,
+                title: 'Dev Office Hours Presentation 10/2',
+                image: 'DevelArticles/cart2.png',
+                link: 'https://docs.google.com/presentation/d/1bd3UDjm5yKY7_2dIgej8kAA7X_DbngcnUQjAma23d1k/edit?slide=id.g34c7c0c3bed_0_16#slide=id.g34c7c0c3bed_0_16'
+            },
+            {
+                id: 5,
+                title: 'Developer Office Hours Recap by RamzesVII 02.10',
+                image: 'DevelArticles/cart3.png',
+                link: 'https://x.com/r_ladik/status/1973805553957941507'
+            },
+            {
+                id: 6,
+                title: 'Dev Office Hours Presentation 9/04',
+                image: 'DevelArticles/cart4.png',
+                link: 'https://docs.google.com/presentation/d/10A00PWv6gQL1FTm04xmPJJCtJMCbLbE85Angd-OqADc/edit?slide=id.g3445fc82bdc_0_105#slide=id.g3445fc82bdc_0_105'
+            },
+            {
+                id: 7,
+                title: 'Developer Office Hours Recap by RamzesVII 04.09',
+                image: 'DevelArticles/cart5.png',
+                link: 'https://x.com/r_ladik/status/1963672274227314689'
+            },
+            {
+                id: 8,
+                title: 'Dev Office Hours Presentation 8/28',
+                image: 'DevelArticles/cart6.png',
+                link: 'https://docs.google.com/presentation/d/1LDkXjcPol7mQIi6KvXz4MGeskY-aPDijU7b6AVYklUU/edit?slide=id.g3445fc82bdc_0_105#slide=id.g3445fc82bdc_0_105'
+            },
+            {
+                id: 9,
+                title: 'Developer Office Hours Recap by RamzesVII 28.08',
+                image: 'DevelArticles/cart7.png',
+                link: 'https://x.com/r_ladik/status/1961157623719293040'
+            },
+            {
+                id: 10,
+                title: 'Dev Office Hours Presentation 8/28',
+                image: 'DevelArticles/c8.png',
+                link: 'https://x.com/StoryEngs/status/1961166414707913107'
+            },
+            {
+                id: 11,
+                title: '5-minute recap of Story Developer Office Hours 08/28 by Unity Nodes',
+                image: 'DevelArticles/c9.png',
+                link: 'https://x.com/StoryEngs/status/1961166414707913107'
+            }
+
+        ];
+    }
+
+    init() {
+        this.renderDevelArticlesContent();
+    }
+
+    renderDevelArticlesContent() {
+        const networkContainer = document.getElementById('network-info');
+        const mainContainer = document.getElementById('main-content');
+
+        if (!networkContainer || !mainContainer) return;
+
+        networkContainer.innerHTML = '';
+        mainContainer.innerHTML = '';
+
+        mainContainer.innerHTML = `
+            <div class="max-w-7xl mx-auto px-6">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 items-start">
+                    ${this.develArticles.map(article => this.renderArticleCard(article)).join('')}
+                </div>
+            </div>
+        `;
+    }
+
+    renderArticleCard(article) {
+        return `
+            <div class="w-full min-w-[300px] hover:scale-105 transition-transform duration-300 cursor-pointer h-full flex flex-col" onclick="window.open('${article.link}', '_blank')">
+                <div class="cartDev overflow-hidden mb-3 w-full flex-shrink-0">
+                    <img src="${article.image}" alt="${article.title}" class="w-full h-40 object-cover rounded-lg" onerror="this.src='media/about.png'">
+                </div>
+                <div class="w-full flex-grow">
+                    <h3 class="font-bold text-sm mb-1 leading-tight">
+                        ${article.title}
+                    </h3>
+                    <p class="text-xs leading-relaxed">
+                        ${article.subtitle || 'Read more about this topic'}
+                    </p>
+                </div>
+            </div>
+        `;
+    }
+}
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	серв := http.NewServeMux()
+
+	серв.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir("./media"))))
+	серв.Handle("/ValidArticles/", http.StripPrefix("/ValidArticles/", http.FileServer(http.Dir("./ValidArticles"))))
+	серв.Handle("/DevelArticles/", http.StripPrefix("/DevelArticles/", http.FileServer(http.Dir("./DevelArticles"))))
+	серв.Handle("/DevelArticlesTwo/", http.StripPrefix("/DevelArticlesTwo/", http.FileServer(http.Dir("./DevelArticlesTwo"))))
+	серв.Handle("/icon/", http.StripPrefix("/icon/", http.FileServer(http.Dir("./icon"))))
+	серв.Handle("/ComArticles/", http.StripPrefix("/ComArticles/", http.FileServer(http.Dir("./ComArticles"))))
+	серв.Handle("/EducArticles/", http.StripPrefix("/EducArticles/", http.FileServer(http.Dir("./EducArticles"))))
+	серв.Handle("/EducArticlesTwo/", http.StripPrefix("/EducArticlesTwo/", http.FileServer(http.Dir("./EducArticlesTwo"))))
+	серв.Handle("/EducResources/", http.StripPrefix("/EducResources/", http.FileServer(http.Dir("./EducResources"))))
+	серв.Handle("/face/", http.StripPrefix("/face/", http.FileServer(http.Dir("./face"))))
+	серв.Handle("/EducAbout/", http.StripPrefix("/EducAbout/", http.FileServer(http.Dir("./EducAbout"))))
+
+	серв.HandleFunc("/", func(отвечающий http.ResponseWriter, запрос *http.Request) {
+		if запрос.URL.Path != "/" {
+			имяФайла := запрос.URL.Path[1:] + ".html"
+			
+			if _, ошибка := os.Stat(имяФайла); ошибка == nil {
+				http.ServeFile(отвечающий, запрос, имяФайла)
+				return
+			}
+			
+			if запрос.URL.Path == "/Header.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "Header.js")
+				return
+			}
+			
+			if запрос.URL.Path == "/ValidatorContent.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "ValidatorContent.js")
+				return
+			}
+			
+			if запрос.URL.Path == "/ValidatorArticles.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "ValidatorArticles.js")
+				return
+			}
+
+			if запрос.URL.Path == "/DeveloperArticles.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "DeveloperArticles.js")
+				return
+			}
+			
+			if запрос.URL.Path == "/CommunityArticles.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "CommunityArticles.js")
+				return
+			}
+
+			if запрос.URL.Path == "/DeveloperArticlesTwo.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "DeveloperArticlesTwo.js")
+				return
+			}
+
+			if запрос.URL.Path == "/DeveloperContent.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "DeveloperContent.js")
+				return
+			}
+
+			if запрос.URL.Path == "/EducationArticles.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "EducationArticles.js")
+				return
+			}
+
+			if запрос.URL.Path == "/EducationArticlesTwo.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "EducationArticlesTwo.js")
+				return
+			}
+
+			if запрос.URL.Path == "/EducationArticlesResource.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "EducationArticlesResource.js")
+				return
+			}
+			
+            if запрос.URL.Path == "/CommunityContent.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "CommunityContent.js")
+				return
+			}
+
+			
+            if запрос.URL.Path == "/EducationAbout.js" {
+				отвечающий.Header().Set("Content-Type", "application/javascript")
+				http.ServeFile(отвечающий, запрос, "EducationAbout.js")
+				return
+			}
+			
+			if запрос.URL.Path == "/favicon.ico" {
+				отвечающий.Header().Set("Content-Type", "image/x-icon")
+				отвечающий.WriteHeader(http.StatusNoContent)
+				return
+			}
+
+			
+			
+			http.NotFound(отвечающий, запрос)
+			return
+		}
+		
+		http.ServeFile(отвечающий, запрос, "Index.html")
+	})
+
+	http.ListenAndServe(":8080", серв)
+}
